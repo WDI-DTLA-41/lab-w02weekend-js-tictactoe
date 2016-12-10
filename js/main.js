7c52344ba89ecfd0c88cc38b5e754dd7ff7ebf0c
Jake Miller <hillsighed@gmail.com>
2016-12-10 15:56:16 -0800

    adds event listener to table

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
 console.log('linked!')
+
+// Players
+var PlayerX = 'X';
+var PlayerO = 'O';
+
+// Event Listener for Table Cells
+// apply to table itself
+var makePlay = function (event) {
+  if (event.target === event.currentTarget) {
+    event.stopPropagation();
+    return false;
+  };
+
+  console.log(event.target.id);
+};
+
+// Get Table
+$table = document.querySelector('table');
+// Get all the Cells
+$Cells = document.querySelectorAll('td')
+
+$table.addEventListener('click', makePlay);
