be60c18b58c80f36ae3c8b51c89396ad578e8102
Jake Miller <hillsighed@gmail.com>
2016-12-10 19:41:19 -0800

    Changed methods used for score keeping using array as the game board and broke out into functions

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
 console.log('linked!')
 
 // Players
-var PlayerX = 'X';
-var PlayerO = 'O';
+var PlayerX = 1; // X will equal 1
+var PlayerO = 2; // O will equal 2
 
 var previousPlayer;
 var currentPlayer = PlayerX;
 
 $tableCells = document.querySelectorAll('td');
+
+// Using zero to represent cell is available
+var board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+
 
 // Event Listener for Table Cells
 // apply to table itself
@@ -16,24 +21,61 @@ var makePlay = function (event) {
     event.stopPropagation();
     return false;
   };
-
-  if (event.target.innerHTML === "") {
-    event.target.innerHTML = currentPlayer;
-    previousPlayer = currentPlayer;
-    if (previousPlayer === PlayerX) {
-      currentPlayer = PlayerO;
-    } else {
-      currentPlayer = PlayerX;
-    }
-    console.log(previousPlayer);
-    console.log(currentPlayer);
+  if ( board[event.target.id] === 0 ) {
+    console.log('cell not yet played');
+    playerSelection();
+    renderGame();
 
   };
+
 };
+
+
+
 
 // Get Table
 $table = document.querySelector('table');
 $table.addEventListener('click', makePlay);
+
+
+// Determine Player
+var playerSelection = function () {
+  if ( currentPlayer === PlayerX ) {
+      console.log('current player is X');
+      board[event.target.id] = 1;
+      previousPlayer = PlayerX;
+      currentPlayer = PlayerO;
+      return true;
+    }
+
+    if ( currentPlayer === PlayerO ) {
+      console.log('current player is O');
+      board[event.target.id] = 2;
+      previousPlayer = PlayerO;
+      currentPlayer = PlayerX;
+      return true;
+    };
+
+};
+
+
+// render Gameboard
+var renderGame = function () {
+  for (var i = 0; i < board.length; i++) {
+    if ( board[i] === 0 ) {
+    } else if ( board[i] === 1 ) {
+      document.getElementById(i).innerHTML = 'X';
+    } else {document.getElementById(i).innerHTML = 'O';
+    };
+  };
+};
+
+
+
+
+
+
+
 
 
 // Reset Game Board
@@ -43,7 +85,7 @@ $resetBtn = document.querySelector('#resetButton')
 // Reset function
 
 var resetGame = function () {
-  for (var i = 0; i <$tableCells.length; i++) {
+  for (var i = 0; i < $tableCells.length; i++) {
     $tableCells[i].innerHTML = "";
   }
   console.log('reset game board');
