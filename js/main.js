7cebda68c33bc16aef8fa2401986524906e6139b
Rony Armas <rony.lunaset@gmail.com>
2016-12-12 09:03:14 -0800

    css raw js only

---
+++ js/main.js
@@ -0,0 +1,37 @@
+var board = [0, 0, 0,
+             0, 0, 0,
+             0, 0, 0];
+
+
+var board = [
+[null, null, null],
+[null, null, null],
+[null, null, null]
+];
+
+button = document.querySelector('button');
+var player1 = 'x';
+var player2 = 'o';
+
+
+var startGame =  function() {
+ console.log('im here');
+var div = document.getElementById('O');
+div.classList.toggle('O');
+
+};
+
+
+var nextPlayer = function() {
+  square.innerHTML = elemX
+}
+
+cell = document.querySelectorAll('.cell');
+cell.addEventListener('click', startGame)
+
+// if (player1)
+
+
+// var elemX = document.createElement('img');
+// elemX.src = 'assets/X.jpg';
+// document.getElementById('bottomRight').appendChild(elemX);
