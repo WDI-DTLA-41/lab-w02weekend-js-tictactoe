8b4de3c75597a6c4ed8e664f7cd951fd34897c1d
Kristopher Parrillo <kkparrillo@gmail.com>
2016-12-11 16:52:07 -0800

    working colors

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var playerOne = document.querySelector('#playerOne');
 var playerTwo = document.querySelector('#playerTwo');
 var gameboard = document.querySelector('#gameboard');
 var resetBtn = document.querySelector('#reset');
-
+var playerTurn = document.querySelector('h2');
 
 // handle for when player button is clicked
 var handlePlayerOne = function(evt) {
@@ -19,11 +19,13 @@ var handlePlayerTwo = function(evt) {
 
 // handle to create either X or O on the board
 var handleX = function(evt) {
-  if (evt.target.textContent = '') {
+  if (evt.target.textContent = ' ') {
     evt.target.textContent = 'X';
   }
   winConditionX();
   gameboard.removeEventListener('click', handleX);
+  playerTurn.textContent = "Player Two's turn";
+  evt.target.classList.toggle('red');
 }
 
 var handleO = function(evt) {
@@ -32,64 +34,54 @@ var handleO = function(evt) {
   }
   winConditionO();
   gameboard.removeEventListener('click', handleO);
+  playerTurn.textContent = "Player One's turn";
+  evt.target.classList.toggle('blue');
 }
 
 // handle the reset of the board
 var handleReset = function() {
   for(var i = 0; i < allBoxes.length; i++) {
   allBoxes[i].textContent = '';
+  allBoxes[i].classList.remove('red');
+  allBoxes[i].classList.remove('blue');
   }
+  playerTurn.textContent = "Player One's turn";
 }
 
+// win conditions for player one and two
 var winConditionX = function() {
   if ((((allBoxes[0].textContent) && (allBoxes[1].textContent) === 'X') && ((allBoxes[2].textContent) === 'X')) ||
     (((allBoxes[3].textContent) && (allBoxes[4].textContent) === 'X') && ((allBoxes[5].textContent) === 'X')) ||
     (((allBoxes[6].textContent) && (allBoxes[7].textContent) === 'X') && ((allBoxes[8].textContent) === 'X'))) {
-      return console.log("Player One Wins!");
+      return alert("Player One Wins!");
   } else if
     ((((allBoxes[0].textContent) && (allBoxes[3].textContent) === 'X') && ((allBoxes[6].textContent) === 'X')) ||
     (((allBoxes[1].textContent) && (allBoxes[4].textContent) === 'X') && ((allBoxes[7].textContent) === 'X')) ||
     (((allBoxes[2].textContent) && (allBoxes[5].textContent) === 'X') && ((allBoxes[8].textContent) === 'X'))) {
-      return console.log("Player One Wins!");
+      return alert("Player One Wins!");
   } else if
     ((((allBoxes[0].textContent) && (allBoxes[4].textContent) === 'X') && ((allBoxes[8].textContent) === 'X')) ||
     (((allBoxes[2].textContent) && (allBoxes[4].textContent) === 'X') && ((allBoxes[2].textContent) === 'X')))  {
-      return console.log("Player One Wins!");
+      return alert("Player One Wins!");
   }
 };
 
 var winConditionO = function() {
-  if ((((allBoxes[0].textContent) && (allBoxes[1].textContent) === 'X') && ((allBoxes[2].textContent) === 'X')) ||
-    (((allBoxes[3].textContent) && (allBoxes[4].textContent) === 'X') && ((allBoxes[5].textContent) === 'X')) ||
-    (((allBoxes[6].textContent) && (allBoxes[7].textContent) === 'X') && ((allBoxes[8].textContent) === 'X'))) {
-      return console.log("Player Two Wins!");
+  if ((((allBoxes[0].textContent) && (allBoxes[1].textContent) === 'O') && ((allBoxes[2].textContent) === 'O')) ||
+    (((allBoxes[3].textContent) && (allBoxes[4].textContent) === 'O') && ((allBoxes[5].textContent) === 'O')) ||
+    (((allBoxes[6].textContent) && (allBoxes[7].textContent) === 'O') && ((allBoxes[8].textContent) === 'O'))) {
+      return alert("Player Two Wins!");
   } else if
-    ((((allBoxes[0].textContent) && (allBoxes[3].textContent) === 'X') && ((allBoxes[6].textContent) === 'X')) ||
-    (((allBoxes[1].textContent) && (allBoxes[4].textContent) === 'X') && ((allBoxes[7].textContent) === 'X')) ||
-    (((allBoxes[2].textContent) && (allBoxes[5].textContent) === 'X') && ((allBoxes[8].textContent) === 'X'))) {
-      return console.log("Player Two Wins!");
+    ((((allBoxes[0].textContent) && (allBoxes[3].textContent) === 'O') && ((allBoxes[6].textContent) === 'O')) ||
+    (((allBoxes[1].textContent) && (allBoxes[4].textContent) === 'O') && ((allBoxes[7].textContent) === 'O')) ||
+    (((allBoxes[2].textContent) && (allBoxes[5].textContent) === 'O') && ((allBoxes[8].textContent) === 'O'))) {
+      return alert("Player Two Wins!");
   } else if
-    ((((allBoxes[0].textContent) && (allBoxes[4].textContent) === 'X') && ((allBoxes[8].textContent) === 'X')) ||
-    (((allBoxes[2].textContent) && (allBoxes[4].textContent) === 'X') && ((allBoxes[2].textContent) === 'X')))  {
-      return console.log("Player Two Wins!");
+    ((((allBoxes[0].textContent) && (allBoxes[4].textContent) === 'O') && ((allBoxes[8].textContent) === 'O')) ||
+    (((allBoxes[2].textContent) && (allBoxes[4].textContent) === 'O') && ((allBoxes[2].textContent) === 'O')))  {
+      return alert("Player Two Wins!");
   }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 // event listeners for players
